070b173210b62bff6f3893863315acb398fe9097
LahLama <jamesihlenfeldt1@gmail.com>
2025-07-27 16:51:48 +0200

    movementViewing | cameraMovement needs fixing

--- OurGame/Assets/Scripts/Player/JamesMovement.cs
+++ OurGame/Assets/Scripts/Player/JamesMovement.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -6,18 +7,25 @@ using UnityEngine.Rendering;
 public class PlayerMovement : MonoBehaviour
 {
     //https://www.youtube.com/watch?v=BxIIg639KpM
+
+    [Header("Movement Settings")]
     public float moveSpeed = 5f;
-    public float lookSensitivity = 2f;
-    public Vector2 LookVector;
-    public Vector3 rotation;
     public Vector2 _moveDirection;
 
+    [Header("Player Rotation")]
+    public Vector2 LookVector;
+    public Vector3 rotation;
+    public float lookSensitivity = 50f;
 
+    [Header("Components")]
     public CharacterController characterController;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        rotation = transform.localEulerAngles;
+
+        Cursor.visible = true; // Hide the cursor
     }
     void Update()
     {
@@ -31,23 +39,29 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-    public void OnLook(InputAction.CallbackContext context) //this is how we read the input
-    {
-        LookVector = context.ReadValue<Vector2>();
-
-    }
-
-    private void RotatePlayer()
-    {
-        rotation.y += LookVector.x * lookSensitivity * Time.deltaTime;
-        transform.localEulerAngles = rotation;
-    }
 
     private void MovePlayer()
     {
         Vector3 move = transform.right * _moveDirection.x + transform.forward * _moveDirection.y;
         characterController.Move(move * moveSpeed * Time.deltaTime);
     }
+
+    public void OnLook(InputAction.CallbackContext context) //this is how we read the input
+    {
+        LookVector = context.ReadValue<Vector2>();
+        print("LookVector: " + LookVector);
+    }
+
+    private void RotatePlayer()
+    {
+        /*print("Rotation: " + rotation);
+        print("LookVector: " + LookVector.x);
+        print("lookSensitivity: " + lookSensitivity);
+       */
+        rotation.y += LookVector.x * lookSensitivity;
+        transform.rotation = Quaternion.Euler(new Vector3(0, rotation.y, 0));
+    }
+
 
 
 
