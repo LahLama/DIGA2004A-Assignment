e56a1dd4b3e456a9e9eadf5ef36202f77e0ce813
LahLama <jamesihlenfeldt1@gmail.com>
2025-07-29 10:26:15 +0200

    Generated C# class for new input system

--- OurGame/Assets/Scripts/Player/JamesMovement.cs
+++ OurGame/Assets/Scripts/Player/JamesMovement.cs
@@ -1,37 +1,24 @@
-using Unity.Mathematics;
-using UnityEditor.Rendering.LookDev;
 using UnityEngine;
-using UnityEngine.EventSystems;
+
 using UnityEngine.InputSystem;
-using UnityEngine.Rendering;
 
 public class PlayerMovement : MonoBehaviour
 {
     //https://www.youtube.com/watch?v=BxIIg639KpM
-
-    [Header("Movement Settings")]
     public float moveSpeed = 5f;
+    public float lookSensitivity = 2f;
+    public Vector2 LookVector;
+    public Vector3 _lookDirection;
     public Vector2 _moveDirection;
 
-    [Header("Player Rotation")]
-    public Vector2 LookVector;
-    public Vector3 rotation;
-    public float lookSensitivity = 50f;
 
-    [Header("Components")]
     public CharacterController characterController;
-    private InputActionAsset inputActions;
-    private InputAction lookAction;
+    private PlayerControls playerControls;
+    private InputAction lookDirectionAction;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        rotation = transform.localEulerAngles;
-        lookAction = inputActions.FindActionMap("Gameplay").FindAction("Look");
-
-        lookAction.performed += context => LookVector = context.ReadValue<Vector2>();
-        lookAction.canceled += context => LookVector = Vector2.zero;
-
     }
     void Update()
     {
@@ -45,21 +32,23 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
+    public void OnLook(InputAction.CallbackContext context) //this is how we read the input
+    {
+        LookVector = context.ReadValue<Vector2>();
+
+    }
+
+    private void RotatePlayer()
+    {
+        _lookDirection.y += LookVector.x * lookSensitivity * Time.deltaTime;
+        transform.localEulerAngles = _lookDirection;
+    }
 
     private void MovePlayer()
     {
         Vector3 move = transform.right * _moveDirection.x + transform.forward * _moveDirection.y;
         characterController.Move(move * moveSpeed * Time.deltaTime);
     }
-
-
-
-    private void RotatePlayer()
-    {
-        float mouseX = LookVector.x * lookSensitivity;
-        transform.Rotate(0, mouseX, 0);
-    }
-
 
 
 
