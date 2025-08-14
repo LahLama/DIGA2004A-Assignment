87bf31e1517069731d628dda0f1fc003c3742b26
LahLama <jamesihlenfeldt1@gmail.com>
2025-08-14 15:01:37 +0200

    Added in-world texts

--- OurGame/Assets/Scripts/Player/HideAndShowPlayer.cs
+++ OurGame/Assets/Scripts/Player/HideAndShowPlayer.cs
@@ -26,6 +26,7 @@ public class HideAndShowPlayer : MonoBehaviour
         playerHeight = player.GetComponent<CharacterController>().height;
         _interactor = GetComponent<Interactor>();
         lookFunction = GetComponentInParent<LookFunction>();
+
     }
 
     void Update()
@@ -47,6 +48,8 @@ public class HideAndShowPlayer : MonoBehaviour
             player.gameObject.transform.SetParent(_hitHideObj.collider.gameObject.transform, true); //
 
             player.GetComponent<PlayerMovement>().enabled = false;
+            // player.GetComponent<CharacterController>().detectCollisions = false;
+
             player.transform.localPosition = Vector3.zero;
             _HideObj = _hitHideObj.collider.gameObject;
             _interactor._interactionDelay = 0.5f;
@@ -63,15 +66,18 @@ public class HideAndShowPlayer : MonoBehaviour
 
     public void ShowPlayer()
     {
-
-
         player.transform.SetParent(null, true);
-        player.transform.localPosition = _playerOGpos;
+        player.GetComponent<PlayerMovement>().enabled = true;
         player.transform.rotation = Quaternion.Euler(_playerOGrot.x, _playerOGrot.y + 180f, _playerOGrot.z);
+        player.transform.localPosition = _playerOGpos;
+
+
+        // player.GetComponent<CharacterController>().detectCollisions = true;
+
 
         lookFunction.cameraTransform.GetComponent<Camera>().fieldOfView = 60;
         lookFunction.verticalLookLimit = 90f;
-        player.GetComponent<PlayerMovement>().enabled = true;
+
 
     }
 }
