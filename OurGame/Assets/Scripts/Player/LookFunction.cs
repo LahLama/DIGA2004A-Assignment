c9293fd61367d80e451b8b42faebea65c7e73eda
LahLama <jamesihlenfeldt1@gmail.com>
2025-08-15 09:19:11 +0200

    LookFunction.cs | Tidy Up

--- OurGame/Assets/Scripts/Player/LookFunction.cs
+++ OurGame/Assets/Scripts/Player/LookFunction.cs
@@ -10,41 +10,45 @@ public class LookFunction : MonoBehaviour
     public float verticalLookLimit = 90f;
     public float bobbingAmplitude = 25f; // Sensitivity for camera bobbing
     public float bobbingFrequency = 1f; // Frequency of bobbing
-    private Vector2 _lookInput;
     private float _verticalRotation = 0f;
-    private float startingYPos = 0;
-    public GameObject HandHeldItem;
-    public GameObject cameraTransform;
-    private PlayerMovement playerMovement;
+    private float _startingYPos = 0;
+    private Vector2 _lookInput;
+    private GameObject _handHeldItem;
+    private GameObject _cameraTransform;
+    private PlayerMovement _playerMovement;
 
     #endregion
 
+    #region UnityFunctions
     private void Awake()
     {
-
-        playerMovement = GetComponent<PlayerMovement>();
-        Cursor.lockState = CursorLockMode.Locked;
-        //Cursor.visible = false;
-        startingYPos = HandHeldItem.transform.localPosition.y;
+        _playerMovement = GetComponent<PlayerMovement>();
+        _cameraTransform = GameObject.FindWithTag("MainCamera");
+        _handHeldItem = GameObject.FindWithTag("HoldingPos");
+        Cursor.lockState = CursorLockMode.Locked;                   //Lock the cursor to the screen
+        _startingYPos = _handHeldItem.transform.localPosition.y;    //Get the intial postion of the object that will be held
     }
     private void Update()
     {
         HandleLook();
     }
+    #endregion
+
+
+    #region NewInputSystem
     public void OnLook(InputAction.CallbackContext context)
+
     {
         _lookInput = context.ReadValue<Vector2>();
     }
+    #endregion
+
 
     public void BobCamera()
-    { //Bobbing
-
-        float __bobbingAmount = Mathf.Sin(Time.time * (bobbingFrequency + playerMovement.moveSpeed / 2)) * bobbingAmplitude;
-
-        //cameraTransform.transform.localRotation = Quaternion.Euler(cameraTransform.transform.localRotation.eulerAngles.x, cameraTransform.transform.localRotation.eulerAngles.y, __bobbingAmount);
-        HandHeldItem.transform.localPosition = new Vector3(HandHeldItem.transform.localPosition.x, startingYPos + __bobbingAmount, HandHeldItem.transform.localPosition.z); // Adjust hand position based on bobbing
-
-
+    {
+        //Bob the player's hands in a sinosoidal wave along the y-axis
+        float __bobbingAmount = Mathf.Sin(Time.time * (bobbingFrequency + _playerMovement.moveSpeed / 2)) * bobbingAmplitude;
+        _handHeldItem.transform.localPosition = new Vector3(_handHeldItem.transform.localPosition.x, _startingYPos + __bobbingAmount, _handHeldItem.transform.localPosition.z);
     }
 
 
@@ -52,11 +56,9 @@ public class LookFunction : MonoBehaviour
     {
         float mouseX = _lookInput.x * lookSensitivity;
         float mouseY = _lookInput.y * lookSensitivity;
-
         _verticalRotation -= mouseY;
         _verticalRotation = Mathf.Clamp(_verticalRotation, -verticalLookLimit, verticalLookLimit);
-
-        cameraTransform.transform.localRotation = Quaternion.Euler(_verticalRotation, 0f, cameraTransform.transform.localRotation.eulerAngles.z);
+        _cameraTransform.transform.localRotation = Quaternion.Euler(_verticalRotation, 0f, _cameraTransform.transform.localRotation.eulerAngles.z);
         transform.Rotate(Vector3.up * mouseX);
     }
 }
