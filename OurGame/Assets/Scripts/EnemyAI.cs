a2d3c9fddf5e2a15e038a2c56f567c64c9d3a87b
LahLama <jamesihlenfeldt1@gmail.com>
2025-09-16 18:46:08 +0200

    Started on EnemyAi.cs and NavMesh

---
+++ OurGame/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask ground;
+    public LayerMask playerLayer;
+}
