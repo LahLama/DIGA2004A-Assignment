2974617cf458d494c6e6da0a12af317396e64c2f
LahLama <jamesihlenfeldt1@gmail.com>
2025-10-06 22:34:24 +0200

    Player is now forced to look at girl when exiting

---
+++ OurGame/Assets/Scripts/Levels/Tutorial/TutForceLook.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class TutForceLook : MonoBehaviour
+{
+    public DialougeState startDialougeScript;
+    public Transform girlEyes;
+    private Transform camera;
+    void Awake()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("HEY LOOOK AT ME");
+        camera.LookAt(girlEyes);
+        Vector3 euler = camera.rotation.eulerAngles;
+        startDialougeScript.StartDialouge();
+        camera.rotation = Quaternion.Euler(euler.x, euler.y, 0);
+        this.gameObject.SetActive(false);
+    }
+}
