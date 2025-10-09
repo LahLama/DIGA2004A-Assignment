4461fe476391d27ded819e3f6b7141a04b1126aa
LahLama <jamesihlenfeldt1@gmail.com>
2025-10-09 15:43:46 +0200

    Door Animation Foundation

--- OurGame/Assets/Scripts/Interactions/interactionLamp.cs
+++ OurGame/Assets/Scripts/Interactions/interactionLamp.cs
@@ -6,10 +6,8 @@ public class interactionLamp : MonoBehaviour, IInteractables
 
     public void Interact()
     {
-        transform.GetChild(0).TryGetComponent<Light>(out Light lampLight);
-        if (lampLight != null)
-        {
-            lampLight.enabled = !lampLight.enabled;
-        }
+        //Show paper on screen.
+
+
     }
 }
