56b111c0f7db55d3a73e962045b6b754ab04e181
LahLama <jamesihlenfeldt1@gmail.com>
2025-10-15 19:46:39 +0200

    Wake Up Animation

---
+++ OurGame/Assets/Scripts/Player/Awakening.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Awakening : MonoBehaviour
+{
+    Animation animation;
+    PlayerMovement playerMovement;
+    LookFunction lookFunction;
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        lookFunction = GetComponent<LookFunction>();
+        animation = GetComponent<Animation>();
+        playerMovement.enabled = false;
+        lookFunction.enabled = false;
+
+    }
+
+
+    void Update()
+    {
+        if (animation.)
+    }
+
+}
