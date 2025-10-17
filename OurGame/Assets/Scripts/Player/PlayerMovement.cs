1d9a873dfc67536eaddbce9833e8a5290c414269
alexM2232 <2886002@students.wits.ac.za>
2025-10-17 02:38:41 +0200

    materials for walls

--- OurGame/Assets/Scripts/Player/PlayerMovement.cs
+++ OurGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -119,6 +119,7 @@ public class PlayerMovement : MonoBehaviour
             if (_moveInput.magnitude > 0.1f)
             {
                 _sprintTimer -= Time.deltaTime;
+                sprintBar.value = 1 - (_sprintTimer / 4); 
                 sprintBar.size = 1 - (_sprintTimer / 4);
                 //Debug.Log("--" + (int)_   sprint}Timer);
             }
@@ -217,7 +218,8 @@ public class PlayerMovement : MonoBehaviour
 
             //Decrease the sprintTimer
             _sprintTimer += Time.deltaTime;
-            sprintBar.size = 1 - (_sprintTimer / 4);
+            sprintBar.value = 1 - (_sprintTimer / 4);
+            
         }
     }
 
