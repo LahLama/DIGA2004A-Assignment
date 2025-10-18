c42595f42d18d1cc3e8cfdbce650e1450f114279
LahLama <jamesihlenfeldt1@gmail.com>
2025-10-18 20:53:45 +0200

    BugFixes.scene | Pickup Tutorial Blocks

--- OurGame/Assets/Scripts/Player/PickUpSystem.cs
+++ OurGame/Assets/Scripts/Player/PickUpSystem.cs
@@ -100,8 +100,12 @@ public class PickUpSystem : MonoBehaviour
 
 
 
-            //To ensure the object doesnt visually clip through walls
-            pickUpObj.layer = LayerMask.NameToLayer("holdingMask");
+            // To ensure the object and all its children don't visually clip through walls
+            int holdingLayer = LayerMask.NameToLayer("holdingMask");
+            foreach (Transform t in pickUpObj.GetComponentsInChildren<Transform>(true))
+            {
+                t.gameObject.layer = holdingLayer;
+            }
             controller.RumblePusle(0.2f, 0.2f, 0.2f);
         }
         else
@@ -121,7 +125,12 @@ public class PickUpSystem : MonoBehaviour
             equipedObj.gameObject.AddComponent<Rigidbody>();
 
             //Reset so that the player cant see the objects through walls
-            equipedObj.gameObject.layer = LayerMask.NameToLayer("pickUpMask");
+
+            int pickUpLayer = LayerMask.NameToLayer("pickUpMask");
+            foreach (Transform t in equipedObj.GetComponentsInChildren<Transform>(true))
+            {
+                t.gameObject.layer = pickUpLayer;
+            }
 
             // Place object infront of player
             Vector3 equipObjPos = equipedObj.transform.localPosition;
@@ -158,8 +167,11 @@ public class PickUpSystem : MonoBehaviour
             equipObjPos = new Vector3(equipObjPos.x, equipObjPos.y + 1, equipObjPos.z);
             equipedObj.SetParent(_pickUpsContatiner, true);
 
-            equipedObj.gameObject.layer = LayerMask.NameToLayer("pickUpMask");
-
+            int pickUpLayer = LayerMask.NameToLayer("pickUpMask");
+            foreach (Transform t in equipedObj.GetComponentsInChildren<Transform>(true))
+            {
+                t.gameObject.layer = pickUpLayer;
+            }
 
             equipedObj.gameObject.transform.localScale = _equippedItemScale;
             equipedObj.gameObject.transform.rotation = _equippedItemRotation;
