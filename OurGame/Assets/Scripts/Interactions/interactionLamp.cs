6a29cd84383eebe173b3ad446556107662a3936d
Erist-Teris <2803324@students.wits.ac.za>
2025-10-30 20:48:29 +0200

    imported new audio and added sound to the lamps and  horse toy

--- OurGame/Assets/Scripts/Interactions/interactionLamp.cs
+++ OurGame/Assets/Scripts/Interactions/interactionLamp.cs
@@ -2,12 +2,25 @@ using UnityEngine;
 
 public class interactionLamp : MonoBehaviour, IInteractables
 {
-
+    public string lampOnSound = "LampOn";   // Name of the "on" sound clip in Resources
+    public string lampOffSound = "LampOff"; // Name of the "off" sound clip in Resources
 
     public void Interact()
     {
         Light light = transform.GetChild(0).GetComponent<Light>();
-        light.enabled = !light.isActiveAndEnabled;
+        bool isTurningOn = !light.isActiveAndEnabled;
+        light.enabled = isTurningOn;
 
+        // Load and play appropriate sound
+        string clipName = isTurningOn ? lampOnSound : lampOffSound;
+        AudioClip clip = Resources.Load<AudioClip>(clipName);
+        if (clip != null)
+        {
+            SoundManager.Instance.Play(clip);
+        }
+        else
+        {
+            Debug.LogWarning($"Lamp sound '{clipName}' not found in Resources.");
+        }
     }
 }
