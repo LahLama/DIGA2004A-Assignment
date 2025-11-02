c652ea6253e02d35ab12449a03161e139fe5e3e9
Erist-Teris <2803324@students.wits.ac.za>
2025-11-02 20:01:11 +0200

    updated script for falling object animation

--- OurGame/Assets/Scripts/Interactions/PlayerInteractionFallObject.cs
+++ OurGame/Assets/Scripts/Interactions/PlayerInteractionFallObject.cs
@@ -2,21 +2,33 @@ using UnityEngine;
 
 public class PlayerInteractionFallObject : MonoBehaviour
 {
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public string fallSoundName = "Fall"; // Name of the sound clip in Resources
+
     void Start()
     {
         
     }
-     void OnTriggerEnter(Collider other)
+
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            this.GetComponent<interactionAnimation>().Interact ();
-        }
+            // Trigger animation
+            GetComponent<interactionAnimation>().Interact();
 
+            // Play sound
+            AudioClip clip = Resources.Load<AudioClip>(fallSoundName);
+            if (clip != null)
+            {
+                SoundManager.Instance.Play(clip);
+            }
+            else
+            {
+                Debug.LogWarning($"Sound clip '{fallSoundName}' not found in Resources.");
+            }
+        }
     }
-    // Update is called once per frame
+
     void Update()
     {
         
