ff07965a6b9342b436584d9cffebe438b17287dc
Erist-Teris <2803324@students.wits.ac.za>
2025-11-03 12:22:43 +0200

    Added missing gameobjects in inspector and updated the enddemo script

--- OurGame/Assets/Scripts/Levels/EndGame/EndDemo.cs
+++ OurGame/Assets/Scripts/Levels/EndGame/EndDemo.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 public class EndDemo : MonoBehaviour, IInteractables
 {
+    [SerializeField] private VideoPlayer endDemoVideoPlayer;
+
     public void Interact()
     {
         EndDemoScreen();
@@ -9,6 +12,14 @@ public class EndDemo : MonoBehaviour, IInteractables
 
     private void EndDemoScreen()
     {
-        Debug.Log("Transistion to the END VIDEO here");
+        if (endDemoVideoPlayer != null)
+        {
+            endDemoVideoPlayer.Play();
+            Debug.Log("Playing end demo video...");
+        }
+        else
+        {
+            Debug.LogWarning("End demo video player not assigned.");
+        }
     }
 }
